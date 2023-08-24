f6fa1f44905aac7af93283fd16ecf9abee119ef3
Rishabh1260 <rishabhjat1260@gmail.com>
2023-08-25 01:39:20 +0530

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     env : {
         MONGODB_URI : "mongodb+srv://Xrishabh:5LDTkaA3gaj70N7l@cluster0.eijacrx.mongodb.net/"
     }
